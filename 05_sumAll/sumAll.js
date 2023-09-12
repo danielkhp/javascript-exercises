bc02310cfd72308ed4ed97a8fb025adc4a1203d2
Daniel <danielkhp@gmail.com>
2023-09-12 16:18:11 -0500

    Add logic to detect non-number arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,8 @@
 const sumAll = function(int1, int2) {
+  if (typeof int1 !== 'number' || typeof int2 !== 'number') {
+    return 'ERROR'
+  }
+
   let sum = 0
 
   for (let i = int1; i <= int2; i++) {
