e88fd5388b18cbf5be0dc7e58b84d8a4be50ea25
Daniel <danielkhp@gmail.com>
2023-09-12 16:04:27 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elems) {
+  let array = arr
 
+  for (const elem of elems) {
+    let index = array.indexOf(elem)
+
+    if (index !== -1) {
+      array.splice(index, 1)
+    }
+  }
+
+  return array
 };
 
 // Do not edit below this line
