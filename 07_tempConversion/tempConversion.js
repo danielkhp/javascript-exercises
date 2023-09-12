1ecd453819a18b06b9efcb56908d505775f48148
Daniel <danielkhp@gmail.com>
2023-09-12 17:07:03 -0500

    Add logic to convert .toFixed string back to number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   let celsius = (fahrenheit - 32) * (5 / 9)
-  return Number.isInteger(celsius) ? celsius : celsius.toFixed(1)
+  return Number.isInteger(celsius) ? celsius : +celsius.toFixed(1)
 };
 
 const convertToFahrenheit = function(celsius) {
-  let fahrenheit = celsisus * (9 / 5) + 32
-  return Number.isInteger(fahrenheit) ? fahrenheit : fahrenheit.toFixed(1)
+  let fahrenheit = celsius * (9 / 5) + 32
+  return Number.isInteger(fahrenheit) ? fahrenheit : +fahrenheit.toFixed(1)
 };
 
 // Do not edit below this line
