63493d8d45deaf7fe648ce1dac8be1d119fdd94e
Daniel <danielkhp@gmail.com>
2023-09-12 15:53:40 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return 'ERROR'
+  }
 
+  let finalStr = ''
+  
+  for (let i = 0; i < num; i++) {
+    finalStr = finalStr.concat(string)
+  }
+
+  return finalStr
 };
 
 // Do not edit below this line
